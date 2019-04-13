f4e66d59d5665bf5e2c2e97d0b657eb74e8ca97e
Ghadeer-Alkhataybeh <eng.ghadeer.alkhataybeh@gmail.com>
2019-04-14 00:36:41 +0300

    <feat>repeat every 5 min

--- server.js
+++ server.js
@@ -35,37 +35,39 @@ app.get("/", (req, res) => {
     "http://api.apixu.com/v1/current.json?key=2cb6b9c413914d02bc775202180507&q=Dubai";
   let promisedData = getData(url);
 
-  promisedData
-    .then(result => {
-      let weath = {
-        time: result.location.localtime,
-        celsiusTemp: result.current.temp_c,
-        weatherStatus: result.current.condition.text,
-        windSpeed: result.current.wind_kph
-      };
-      res.send(weath);
-      weather(weath);
-      // // Query to insert the weather information
-      // let query = `insert into weather values
-      //     (null,\"${weath.time}\",\"${weath.weatherStatus}\",\"${
-      //   weath.celsiusTemp
-      // }\",\"${weath.windSpeed}\")`;
+  setInterval(() => {
+    promisedData
+      .then(result => {
+        let weath = {
+          time: result.location.localtime,
+          celsiusTemp: result.current.temp_c,
+          weatherStatus: result.current.condition.text,
+          windSpeed: result.current.wind_kph
+        };
+        // res.send(weath);
+        weather(weath);
+        // // Query to insert the weather information
+        // let query = `insert into weather values
+        //     (null,\"${weath.time}\",\"${weath.weatherStatus}\",\"${
+        //   weath.celsiusTemp
+        // }\",\"${weath.windSpeed}\")`;
 
-      // // insert weather information to the database
-      // dbConnection.db.query(query, function(err, result) {
-      //   if (result) {
-      //     res.send("Saved");
-      //   } else {
-      //     res.send("Error");
-      //   }
-      // });
-      //   console.log(weath);
-    })
-    .then(() => {
-      console.log("Process finished, waiting 5 minutes till the next update");
-    })
+        // // insert weather information to the database
+        // dbConnection.db.query(query, function(err, result) {
+        //   if (result) {
+        //     res.send("Saved");
+        //   } else {
+        //     res.send("Error");
+        //   }
+        // });
+        //   console.log(weath);
+      })
+      .then(() => {
+        console.log("Process finished, waiting 5 minutes till the next update");
+      })
 
-    .catch(err => console.log(err));
+      .catch(err => console.log(err));
+  }, 1000 * 60 * 5);
 });
 
 app.listen(port, () => console.log(`app listening on port ${port}!`));
